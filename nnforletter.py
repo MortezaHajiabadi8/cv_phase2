65669c218b5ef68be8a68be40ce0d1dfcff8eb63
mortezahajiabadi <hajiabadi1377@gmail.com>
2020-07-27 02:06:43 +0430

    load images

---
+++ nnforletter.py
@@ -0,0 +1,30 @@
+import keras
+import glob
+from tqdm import tqdm
+import os
+import cv2
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+import re
+
+num_classes = 32
+EPOCHS = 25
+BS = 32
+
+train_dirs = glob.glob("./dataset/letters/*")
+train_dirs.sort(key = lambda x: int(x.rsplit('/',1)[1]))
+
+
+data = []
+labels = []
+for train_dir in tqdm(train_dirs):
+    imgPaths = glob.glob(train_dir + "/*.jpg")
+    imgPaths.sort()
+    for imgPath in tqdm(imgPaths):
+        image = load_img(imgPath, target_size=(28, 28), grayscale=True)
+        image = img_to_array(image) 
+        data.append(image)
+
+        label = imgPath.split(os.path.sep)[-2]
+        label = int(label)
+        labels.append(label)
